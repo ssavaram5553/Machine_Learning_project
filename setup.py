fd3c095169d23b1fbcf09e1a8a795e65e887cead
Suresh <savaram.sureshkumar@gmail.com>
2025-06-13 13:23:13 -0700

    update req

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ def get_requirements(file_path:str)->List[str]:
   requirements=[]
   with open(file_path) as file_obj:
     requirements = file_obj.readlines()
-    requirements = [req.replace("\n","") for req in requirements]
+    #requirements = [req.replace("\n","") for req in requirements]
+    requirements =[req.strip() for req in requirements]
     if HYPEN_E_DOT in requirements:
       requirements.remove(HYPEN_E_DOT)
 
+  return requirements
 
 setup(name = "AI_ML_PROJECT",
       version="0.0.1",
